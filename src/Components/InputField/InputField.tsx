b636416ae8db5900caa87da07c4b44bdd58bbd83
Vastro-lorde <omatsolaseund@outlook.com>
2022-06-24 02:35:06 +0100

    styled the input

--- src/Components/InputField/InputField.tsx
+++ src/Components/InputField/InputField.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import InputFieldCss from './InputField.module.css';
 
-export const InputField = () => {
-  return <div>InputField</div>;
+interface props {
+  task: string;
+  setTask: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const InputField = ({ task, setTask }: props) => {
+  return (
+    <form action='' className={InputFieldCss.taskInput}>
+      <input type='input' name='task' id='' className={InputFieldCss.inputBox} placeholder='Enter new task' required />
+      <button type='submit' className={InputFieldCss.inputSubmit}>
+        Add
+      </button>
+    </form>
+  );
 };
--- src/Components/Main/Todo.tsx
+++ src/Components/Main/Todo.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { InputField } from '../InputField/InputField';
 import TodoCss from './Todo.module.css';
 
 function Todo() {
+  const [task, setTask] = useState<string>('');
+
   return (
     <div className={TodoCss.App}>
       <span className={TodoCss.heading}>Todo App</span>
+      <InputField task={task} setTask={setTask} />
     </div>
   );
 }
